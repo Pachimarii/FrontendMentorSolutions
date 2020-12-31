3f9114b2b0b7730979e85a07ce0e20e926cedaae
Minhui Xie <MXie@cese.fraunhofer.org>
2020-12-31 15:59:41 -0500

    completed the solution to the challenge article-preview-compoenent

---
+++ article-preview-component-master/script.js
@@ -0,0 +1,7 @@
+const button = document.querySelector(".share");
+const popup = document.querySelector(".pop_up");
+
+button.addEventListener("click", function () {
+    button.classList.toggle("active");
+    popup.classList.toggle("hiding");
+});
